5176323b31f1d3a527ae493f352313da3cd635b9
451suman <sumanmushyakhwo@gmail.com>
2024-06-28 23:19:59 +0545

    Add project files.

---
+++ CRUDOPERATION/Controllers/CategoryController.cs
@@ -0,0 +1,49 @@
+﻿using CRUDOPERATION.Data;
+using CRUDOPERATION.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDOPERATION.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;            
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList= _db.categories;
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+     
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+
+
+
+            if(ModelState.IsValid)    
+            {
+               /* if (obj.Name == "suman")
+                {
+                    ModelState.AddModelError("Name", "The Name cannot be 'suman'.");
+                    return View(obj); // Return the view with errors
+                }*/
+                _db.categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+          //if validation code written in model is validat redirect to Index
+          // stay on same page
+
+        }
+    }
+   
+}
---
+++ CRUDOPERATION/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using CRUDOPERATION.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDOPERATION.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) { 
+        }
+        
+        public DbSet<Category> categories { get; set; }
+    }
+}
