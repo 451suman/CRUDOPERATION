14bfb5f59cfc5eff4e5385e73a79d25e3d17c4fb
451suman <sumanmushyakhwo@gmail.com>
2024-06-29 12:50:09 +0545

    create edit butto nin index.cshtml and pass Id to search that id and route then in edit page

--- CRUDOPERATION/Controllers/CategoryController.cs
+++ CRUDOPERATION/Controllers/CategoryController.cs
@@ -44,6 +44,49 @@ namespace CRUDOPERATION.Controllers
           // stay on same page
 
         }
+
+
+        //EDit 
+        public IActionResult Edit(int? id)
+        {
+            if(id== null || id==0)
+            {
+                return RedirectToAction("Index");
+            }
+            var categoryFromDB = _db.categories.Find(id);
+            /*var categoryFormDbFirst = _db.categories.FirstOrDefault(c => c.Id == id);
+            var categoryFormDbFirst = _db.categories.SingleOrDefault(c => c.Id == id); */           
+            if (categoryFromDB == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDB);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+
+
+
+            if (ModelState.IsValid)
+            {
+                /* if (obj.Name == "suman")
+                 {
+                     ModelState.AddModelError("Name", "The Name cannot be 'suman'.");
+                     return View(obj); // Return the view with errors
+                 }*/
+                _db.categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+            //if validation code written in model is validat redirect to Index
+            // stay on same page
+
+        }
     }
    
 }
--- CRUDOPERATION/Models/Category.cs
+++ CRUDOPERATION/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace CRUDOPERATION.Models
 {
@@ -7,8 +8,12 @@ namespace CRUDOPERATION.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        
         public string Name { get; set; }
 
+        [Required]
+        [DisplayName("kasdjl;kasd")]
+        [Range(1, int.MaxValue, ErrorMessage = "Display Order must be greater than 0")]
         public int DisplayOrder { get; set; }
 
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
